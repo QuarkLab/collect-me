fe55b6231da3b3db5ffd48319f6cfc66fe9104db
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-10-08 06:04:43 +0530

    Create Navigations

--- server/web/pages/browse.php
+++ server/web/pages/browse.php
@@ -1,42 +1,67 @@
 
 
+<button id="navigate" type="button" name="button">START</button>
 
 <div id="map" style="width:100%;height:100%;background:yellow"></div>
-
 <script>
 function initMap() {
 
-  window.map = new google.maps.Map(document.getElementById('map'), {
+
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 16,
     center: {lat:8.7563524, lng: 80.4943286}
   });
 
+  window.directionsService = new google.maps.DirectionsService;
+  window.directionsDisplay = new google.maps.DirectionsRenderer;
+  window.directionsDisplay.setMap(map);
+
   setMarkers(map);
+
+  var onChangeHandler = function() {
+    calculateAndDisplayRoute(window.directionsService, window.directionsDisplay, "Iluppayadi, lk", "Vavuniya+Campus,+University+of+Jaffna, lk");
+  };
+
+  document.getElementById('navigate').addEventListener('click', onChangeHandler);
 
 }
 
 function setMarkers(){
   clearMarkers(markers);
   var markers = [];
-  markers.push({name: 'Marker 1', lat: 8.7563524, lng: 80.4943286, type:'paper'});
-  markers.push({name: 'Marker 2', lat: 8.7560028, lng: 80.4919929, type:'metal'});
-  markers.push({name: 'Marker 3', lat: 8.7588532, lng: 80.4927693, type:'plastic'});
-  markers.push({name: 'Marker 4', lat: 8.7585297, lng: 80.4938946, type:'glass'});
-  markers.push({name: 'Marker 5', lat: 8.7575661, lng: 80.4923844, type:'plastic'});
-  markers.push({name: 'Marker 6', lat: 8.762289, lng: 80.489012, type:'metal'});
-  markers.push({name: 'Marker 7', lat: 8.759274, lng: 80.500069, type:'paper'});
-  markers.push({name: 'Marker 8', lat: 8.752112, lng: 80.495436, type:'glass'});
+  markers.push({name: 'Marker 1', lat: 8.7585700, lng: 80.491870, type:'paper', place:'Sewalanka+Foundation, lk'});
+  markers.push({name: 'Marker 2', lat: 8.7626494, lng: 80.490561, type:'metal', place:'Pop+Yarl+Ice+Cream, lk'});
+  markers.push({name: 'Marker 3', lat: 8.758109, lng: 80.490282, type:'plastic', place:'Suvai+Aruvi, lk'});
+  markers.push({name: 'Marker 4', lat: 8.757731, lng: 80.491550, type:'glass', place:'Recharge+Now+Dth+Systems, lk'});
+  markers.push({name: 'Marker 5', lat: 8.758239, lng: 80.500592, type:'plastic', place:'Iluppayadi, lk'});
+  markers.push({name: 'Marker 6', lat: 8.762289, lng: 80.489012, type:'metal', place:'Vithusha+Medi+Clinic'});
+  markers.push({name: 'Marker 7', lat: 8.761160, lng: 80.497254, type:'paper', place:'Vavuniya+Divisional+Secretariat, lk'});
+  markers.push({name: 'Marker 8', lat: 8.754790, lng: 80.495007, type:'glass', place:'Distilleries+Company+Wholesale+Outlet, lk'});
 
   for (m in markers){
     if (document.getElementById('chk-'.concat(markers[m]['type'])).checked){
+      mk = markers[m]['place'];
       markers[m] = new google.maps.Marker({
+        animation: google.maps.Animation.DROP,
         position: {lat:markers[m]['lat'], lng:markers[m]['lng']},
         map: window.map,
+        place: markers[m]['place'],
         title: markers[m]['name'],
         icon: 'images/bin-'.concat(markers[m]['type'], '.png')
       });
+
+      google.maps.event.addListener(markers[m], 'click', (function(mk) {
+        return function() {
+              calculateAndDisplayRoute(window.directionsService, window.directionsDisplay, "Vavuniya+Campus,+University+of+Jaffna, lk", mk);
+        }
+      })(mk));
     }
   }
+  window.markers = markers;
+}
+
+function getWord(event){
+  alert(event);
 }
 
 function clearMarkers(markers){
@@ -45,6 +70,25 @@ function clearMarkers(markers){
       markers[m].setMap(null);
     }
   }
+}
+
+
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay, start, end) {
+  // alert(start);
+  // alert(end);
+  directionsService.route({
+    origin: start,
+    destination: end,
+    travelMode: 'DRIVING'
+  }, function(response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+      // alert(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
 }
 
 </script>
